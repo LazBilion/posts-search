dfdb9be92501026da0bc90f7f15b4a8a9aaa3aef
lazbil <lazbilion@gmail.com>
2019-11-19 11:56:43 +0200

    Post photo thumbnail opens in new tab

--- HTML-Templates/User-post-photo-template.js
+++ HTML-Templates/User-post-photo-template.js
@@ -1,3 +1,3 @@
 export const photoTemplate = obj => `<div class="photo">
-			<img src = "${obj[0].thumbnailUrl}" alt = "${obj[0].title}">
+			<a target="_blank" href="${obj[0].url}"><img src = "${obj[0].thumbnailUrl}" alt = "${obj[0].title}"></a>
 		</div>`;
