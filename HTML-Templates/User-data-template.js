21ea472d090f35e66c701db9a33d9251e10eeb4e
lazbil <lazbilion@gmail.com>
2019-11-21 15:02:53 +0200

    Tweaks in CSS and template JS files to resemble mockup presentation

--- HTML-Templates/User-data-template.js
+++ HTML-Templates/User-data-template.js
@@ -1,26 +1,28 @@
 export const userTemplate = obj => `<div id="user-info">
-			<div class="user-photo">
-			<i class="fab fa-d-and-d"></i>
-			</div>
-			<div class = "user-tab">
-				<div class="user-information">
-					<div class="username">
-						<h1>Username: ${obj.username}</h1>
-					</div>
-					<div class="user-email">
-						<h1>Email: ${obj.email}</h1>
-					</div>
-					<div class="city">
-						<h1>City: ${obj.address.city}<h1>
-					</div>
-					<div class="google-map">
-						<h1><a target="_blank" href = "https://maps.google.com/?ll=${obj.address.geo.lat},${obj.address.geo.lng}">Google Maps geo location</a></h1>
-					</div>
-					<div class="company name">
-						<h1>Company: ${obj.company.name}</h1>
-					</div>
-					<div class="website-link">
-						<h1>Website: ${obj.website}</h1>
+			<div class="user-card">			
+				<div class="user-photo">
+					<i class="fab fa-d-and-d"></i>
+				</div>
+				<div class = "user-tab">
+					<div class="user-information">
+						<div class="username" style="color: white; background-color: rgb(61,91,151);">
+							<h1 >${obj.username}</h1>
+						</div>
+						<div class="user-email" style="color: white; background-color: rgb(61,91,151);">
+							<h1">${obj.email}</h1">
+						</div>
+						<div class="city">
+							<h1>${obj.address.city}<h1>
+						</div>
+						<div class="google-map">
+							<h1><a target="_blank" href = "https://maps.google.com/?ll=${obj.address.geo.lat},${obj.address.geo.lng}">Google Maps geo location</a></h1>
+						</div>
+						<div class="company name">
+							<h1>${obj.company.name}</h1>
+						</div>
+						<div class="website-link">
+							<h1>${obj.website}</h1>
+						</div>
 					</div>
 				</div>
 			</div>
--- HTML-Templates/User-post-data-template.js
+++ HTML-Templates/User-post-data-template.js
@@ -1,11 +1,13 @@
 export const postTemplate = obj => `<div class = "posts">
-<div class="post-title">
-    <h2>${obj.title}<h2>
-</div>
-<div class="post-body">
-    <p>${obj.body}</p>
-</div>
+    <div class ="eachPost">
+        <div class="post-title">    
+            <h2>${obj.title}<h2>
+        </div>
+        <div class="post-body">
+            <p>${obj.body}</p>
+        </div>
+    </div>
 </div>
 <div class="more-info">
-<button type="button">more comments...</button>
-</div>`
+<button type="button">show comments...</button>
+</div>`;
